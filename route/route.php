e589ce47090dd19980730ec522af756ba5d817d8
coreliang <coreliang@163.com>
2018-10-28 10:21:36 +0800

    第三章完结

--- route/route.php
+++ route/route.php
@@ -9,12 +9,26 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-Route::get('think', function () {
-    return 'hello,ThinkPHP5!';
-});
+// Route::get('think', function () {
+//     return 'hello,ThinkPHP5!';
+// });
 
-Route::get('hello/:name', 'index/hello');
+//Route::get('hello/:name', 'index/hello');
+//route::rule('url','实际路径/参数1/值1')
+//Route::get('hello','index/hello');
+// Route::get('hello',function(){
+//     return 'hello closures';
+// });
+//Route::get('hello','index/index/hello');
+// Route::get('hello',function(){
+//     return view('index@index/hello');// 模块@/模板下对应的模板文件夹/对应的模板文件名.html
+// });
+// Route::get('hello/:name',function($name){
+//     return 'hello,'.$name;
+// });
+// return [
 
-return [
-
-];
+// ];
+Route::get('','welcome/index/home');
+Route::get('/help','welcome/index/help');
+Route::get('/about','welcome/index/about');
